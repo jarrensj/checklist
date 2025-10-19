2ee9b8227265a73b5d42f15e40505b09848f482e
Jarren San Jose <sanjose.jarren@gmail.com>
2025-10-18 22:47:30 -0700

    Initial Commit

---
+++ checklist/checklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  checklistApp.swift
+//  checklist
+//
+//  Created by Jarren on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct checklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ checklistTests/checklistTests.swift
@@ -0,0 +1,17 @@
+//
+//  checklistTests.swift
+//  checklistTests
+//
+//  Created by Jarren on 10/18/25.
+//
+
+import Testing
+@testable import checklist
+
+struct checklistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
